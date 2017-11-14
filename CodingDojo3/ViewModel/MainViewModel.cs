a1c2c8090146ecb9633c726de0f9a6ec7df33921
Maximilian Dick <wi16b066@technikum-wien.at>
2017-11-14 11:41:59 +0100

    Datacontext + Time

---
+++ CodingDojo3/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo3.ViewModel
+{
+    public class MainViewModel
+    {
+        string zeit = DateTime.Now.ToLocalTime().ToShortTimeString();
+        string datum = DateTime.Now.ToLocalTime().ToShortDateString();
+
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
